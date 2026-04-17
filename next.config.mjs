c7b4b4fe8c743a545dce33e3c2db0bd5dd6dff0f
Cosmo13911 <tonka4814@gmail.com>
2026-04-17 13:12:53 +0700

    change config

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true, // เพิ่มบรรทัดนี้เพื่อให้รูปภาพแสดงผลได้บน GitHub Pages
   },
-  // ถ้าชื่อ Repo ของน้องไม่ใช่ชื่อเดียวกับ username ให้ใส่ basePath ด้วย
-  basePath: '/Stone_Age_Board_Game_Tool', 
 };
 
 export default nextConfig;
